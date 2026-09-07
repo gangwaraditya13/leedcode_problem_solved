43dfa016df76085bc5d1654541c5145b804fe0b2
gangwaraditya13 <gangwaraditya13@gmail.com>
2026-09-08 00:53:54 +0530

    MaximumAverageSubarrayI

--- src/MaximumAverageSubarrayI.java
+++ src/MaximumAverageSubarrayI.java
@@ -1,4 +1,37 @@
-package PACKAGE_NAME;
-
 public class MaximumAverageSubarrayI {
+    public static void main(String[] args){
+        SolutionMaximumAverageSubarrayI maximumAverageSubarrayI = new SolutionMaximumAverageSubarrayI();
+        double maxAverage = maximumAverageSubarrayI.findMaxAverage(new int[]{1, 12, -5, -6, 50, 3}, 4);
+        System.out.println(maxAverage);
+    }
+}
+
+class SolutionMaximumAverageSubarrayI {
+    public double findMaxAverage(int[] nums, int k) {
+        double avg = 0;
+        int sum = 0;
+        int i = 0;
+        int j = 0;
+
+        while (j < k) {
+            sum += nums[j];
+            j++;
+        }
+        avg = (double) sum / k;
+
+        while (j < nums.length) {
+            sum -= nums[i];
+            i++;
+
+            sum += nums[j];
+            j++;
+
+            double currentAvg = (double) sum / k;
+            if (currentAvg > avg) {
+                avg = currentAvg;
+            }
+        }
+
+        return avg;
+    }
 }
