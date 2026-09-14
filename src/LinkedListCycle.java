ccd81cb3719afd6fb935bd7afcce35cd1c8dc1ff
gangwaraditya13 <gangwaraditya13@gmail.com>
2026-09-15 00:23:11 +0530

    LinkedListCycle

---
+++ src/LinkedListCycle.java
@@ -0,0 +1,24 @@
+public class LinkedListCycle {
+    static public void main(String[] args){
+
+    }
+}
+
+class SolutionLinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
---
+++ src/test/mearge2List.java
@@ -0,0 +1,4 @@
+package test;
+
+public class mearge2List {
+}
