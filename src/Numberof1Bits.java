e8506a0efca5fdbbeef2c0ff250f8bd8fabb1a13
gangwaraditya13 <gangwaraditya13@gmail.com>
2026-09-20 23:49:26 +0530

    Numberof1Bits

--- src/Numberof1Bits.java
+++ src/Numberof1Bits.java
@@ -1,4 +1,19 @@
-package PACKAGE_NAME;
-
 public class Numberof1Bits {
+    public static void main(String[] args){
+        SolutionNumberof1Bits numberof1Bits = new SolutionNumberof1Bits();
+        System.out.println(numberof1Bits.hammingWeight(2147483645));
+    }
+}
+
+
+class SolutionNumberof1Bits {
+    public int hammingWeight(int n) {
+        int count = 0;
+        for (int i = n; i != 0; i = (i >> 1)) {
+            if((i&1) == 1){
+                count++;
+            }
+        }
+        return count;
+    }
 }
