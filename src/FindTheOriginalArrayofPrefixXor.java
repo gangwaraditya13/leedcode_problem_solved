061f99448967feedb30898b323062a5c072beaf5
gangwaraditya13 <gangwaraditya13@gmail.com>
2026-09-21 23:15:30 +0530

    FindTheOriginalArrayofPrefixXor

--- src/FindTheOriginalArrayofPrefixXor.java
+++ src/FindTheOriginalArrayofPrefixXor.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
+import java.util.Arrays;
 
 public class FindTheOriginalArrayofPrefixXor {
+    public static void main(String[] args){
+        SolutionFindTheOriginalArrayofPrefixXor findTheOriginalArrayofPrefixXor = new SolutionFindTheOriginalArrayofPrefixXor();
+        System.out.println(Arrays.toString(findTheOriginalArrayofPrefixXor.findArray(new int[]{13})));
+    }
+}
+
+class SolutionFindTheOriginalArrayofPrefixXor {
+    public int[] findArray(int[] pref) {
+        int[] ans = new int[pref.length];
+        for (int i = 0; i < pref.length; i++) {
+            if(i == 0){
+                ans[i] = pref[i];
+            }else{
+                ans[i] = pref[i] ^ pref[i-1];
+            }
+        }
+
+        return ans;
+    }
 }
