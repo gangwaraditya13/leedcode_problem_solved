decfae16557f84ea51e76c2ce326468f1f6309cb
gangwaraditya13 <gangwaraditya13@gmail.com>
2026-09-22 00:36:18 +0530

    RansomNote

--- src/RansomNote.java
+++ src/RansomNote.java
@@ -1,4 +1,27 @@
-package PACKAGE_NAME;
-
 public class RansomNote {
+    public static void main(String[] args){
+        SolutionRansomNote ransomNot = new SolutionRansomNote();
+        System.out.println(ransomNot.canConstruct("aa","aab"));
+
+    }
+}
+
+class SolutionRansomNote {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] count = new int[26];
+
+        for (char c : magazine.toCharArray()) {
+            count[c - 'a']++;
+        }
+
+        for (char c : ransomNote.toCharArray()) {
+            count[c - 'a']--;
+
+            if (count[c - 'a'] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 }
