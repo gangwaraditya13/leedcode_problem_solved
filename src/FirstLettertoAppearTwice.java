6f9ff56ea31d547129cbb55018571340d13d4f3a
gangwaraditya13 <gangwaraditya13@gmail.com>
2026-09-24 23:10:52 +0530

    FirstLettertoAppearTwice

--- src/FirstLettertoAppearTwice.java
+++ src/FirstLettertoAppearTwice.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
-
 public class FirstLettertoAppearTwice {
+    static public void main(String[] args){
+        SolutionFirstLettertoAppearTwice firstLettertoAppearTwice = new SolutionFirstLettertoAppearTwice();
+        System.out.println(firstLettertoAppearTwice.repeatedCharacter("abccbaacz"));
+    }
 }
+
+class SolutionFirstLettertoAppearTwice {
+    public char repeatedCharacter1(String s) {
+        int i =1;
+        while (i<s.length()){
+            if(s.charAt(i) == s.charAt(i-1)){
+                return s.charAt(i);
+            }
+            i++;
+        }
+
+        return '\0';
+    }
+
+    public char repeatedCharacter(String s) {
+        boolean[] seen = new boolean[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            int index = s.charAt(i) - 'a';
+
+            if (seen[index]) {
+                return s.charAt(i);
+            }
+
+            seen[index] = true;
+        }
+
+        return '\0';
+    }
+
+
+}
+
